93143674d9765a999c2f15d3798177344608361a
danghieu7424 <danghieu7424storage@gmail.com>
2024-12-14 19:44:08 +0700

    add table

--- src/ddtu.js
+++ src/ddtu.js
@@ -102,20 +102,30 @@ function PhongDDTU() {
                     <caption>Bảng</caption>
                     <thead>
                         <tr>
-                            <th>Mã phòng</th>
-                            <th>Tên phòng</th>
+                            <th>Mã Phòng</th>
+                            <th>Tên Phòng</th>
+                            <th>Mã Ngành</th>
+                            <th>Mã Cán Bộ</th>
+                            <th>Giai Đoạn QL</th>
+                            <th>Số Tuần</th>
+                            <th>Giờ Quy Đổi</th>
                         </tr>
                     </thead>
                     <tbody>
                         {loading ? (
                             <tr>
-                                <td colSpan="2">Loading...</td>
+                                <td colSpan="1">Loading...</td>
                             </tr>
                         ) : (
                             data.map((item, index) => (
                                 <tr key={index}>
-                                    <td>{item.MAPH}</td>
-                                    <td>{item.TENP}</td>
+                                    <td>{item.MAPX}</td>
+                                    <td>{item.TENPX || ''}</td>
+                                    <td>{item.MANG || ''}</td>
+                                    <td>{item.MACB || ''}</td>
+                                    <td>{item.GiaiDoanQL || ''}</td>
+                                    <td>{item.Sotu || ''}</td>
+                                    <td>{item.GioQD || ''}</td>
                                 </tr>
                             ))
                         )}
@@ -150,6 +160,67 @@ function TaiSanDDTU() {
             });
     }, []);
 
+    useEffect(() => {
+        /*
+         * @param {HTMLTableElement}
+         * @param {number}
+         * @param {boolean}
+         */
+        function sortTableByColumn(table, column, asc = true) {
+            const dirModifier = asc ? 1 : -1;
+            const tBody = table.tBodies[0];
+            const rows = Array.from(tBody.querySelectorAll("tr"));
+
+            const sortedRows = rows.sort((a, b) => {
+                const aColText = a
+                    .querySelector(`td:nth-child(${column + 1})`)
+                    .textContent.trim();
+                const bColText = b
+                    .querySelector(`td:nth-child(${column + 1})`)
+                    .textContent.trim();
+
+                return aColText > bColText ? 1 * dirModifier : -1 * dirModifier;
+            });
+
+            while (tBody.firstChild) {
+                tBody.removeChild(tBody.firstChild);
+            }
+
+            tBody.append(...sortedRows);
+
+            table
+                .querySelectorAll("th")
+                .forEach((th) =>
+                    th.classList.remove("th-sort-asc", "th-sort-desc")
+                );
+            table
+                .querySelector(`th:nth-child(${column + 1})`)
+                .classList.toggle("th-sort-asc", asc);
+            table
+                .querySelector(`th:nth-child(${column + 1})`)
+                .classList.toggle("th-sort-desc", !asc);
+        }
+
+        document.querySelectorAll("#content_table th").forEach((headerCell) => {
+            headerCell.addEventListener("click", () => {
+                const tableElement =
+                    headerCell.parentElement.parentElement.parentElement;
+                const headerIndex = Array.prototype.indexOf.call(
+                    headerCell.parentElement.children,
+                    headerCell
+                );
+                const currentIsAscending =
+                    headerCell.classList.contains("th-sort-asc");
+
+                sortTableByColumn(
+                    tableElement,
+                    headerIndex,
+                    !currentIsAscending
+                );
+            });
+        });
+    }, []);
+
     return (
         <section id="section__introduce">
             <div id="box_table">
@@ -157,20 +228,42 @@ function TaiSanDDTU() {
                     <caption>Bảng</caption>
                     <thead>
                         <tr>
-                            <th>Mã phòng</th>
-                            <th>Tên phòng</th>
+                            <th>Mã Đồ</th>
+                            <th>Tên Đồ</th>
+                            <th>Thông Số KT</th>
+                            <th>Mã Phòng</th>
+                            <th>Nơi SX</th>
+                            <th>ĐVT</th>
+                            <th>GLGSS</th>
+                            <th>GLGTT</th>
+                            <th>SL Thừa</th>
+                            <th>SL Thiếu</th>
+                            <th>SLSD</th>
+                            <th>Giải Trình</th>
+                            <th>Ghi Chú</th>
                         </tr>
                     </thead>
                     <tbody>
                         {loading ? (
                             <tr>
-                                <td colSpan="2">Loading...</td>
+                                <td colSpan="1">Loading...</td>
                             </tr>
                         ) : (
                             data.map((item, index) => (
                                 <tr key={index}>
-                                    <td>{item.MAPH}</td>
-                                    <td>{item.TENP}</td>
+                                    <td>{item.MADO || ''}</td>
+                                    <td>{item.TENDO || ''}</td>
+                                    <td>{item.THONGSOKT || 'N/A'}</td>
+                                    <td>{item.MAPH || 'N/A'}</td>
+                                    <td>{item.NOISX || ''}</td>
+                                    <td>{item.DVT || 'N/A'}</td>
+                                    <td>{item.GLGSS || '0'}</td>
+                                    <td>{item.GLGTT || '0'}</td>
+                                    <td>{item.SLTHUA || '0'}</td>
+                                    <td>{item.SLTHIEU || '0'}</td>
+                                    <td>{item.SLSD || '0'}</td>
+                                    <td>{item.GIAITRINH || ''}</td>
+                                    <td>{item.GHICHU || ''}</td>
                                 </tr>
                             ))
                         )}
@@ -205,6 +298,67 @@ function TLTaiSanDDTU() {
             });
     }, []);
 
+    useEffect(() => {
+        /*
+         * @param {HTMLTableElement}
+         * @param {number}
+         * @param {boolean}
+         */
+        function sortTableByColumn(table, column, asc = true) {
+            const dirModifier = asc ? 1 : -1;
+            const tBody = table.tBodies[0];
+            const rows = Array.from(tBody.querySelectorAll("tr"));
+
+            const sortedRows = rows.sort((a, b) => {
+                const aColText = a
+                    .querySelector(`td:nth-child(${column + 1})`)
+                    .textContent.trim();
+                const bColText = b
+                    .querySelector(`td:nth-child(${column + 1})`)
+                    .textContent.trim();
+
+                return aColText > bColText ? 1 * dirModifier : -1 * dirModifier;
+            });
+
+            while (tBody.firstChild) {
+                tBody.removeChild(tBody.firstChild);
+            }
+
+            tBody.append(...sortedRows);
+
+            table
+                .querySelectorAll("th")
+                .forEach((th) =>
+                    th.classList.remove("th-sort-asc", "th-sort-desc")
+                );
+            table
+                .querySelector(`th:nth-child(${column + 1})`)
+                .classList.toggle("th-sort-asc", asc);
+            table
+                .querySelector(`th:nth-child(${column + 1})`)
+                .classList.toggle("th-sort-desc", !asc);
+        }
+
+        document.querySelectorAll("#content_table th").forEach((headerCell) => {
+            headerCell.addEventListener("click", () => {
+                const tableElement =
+                    headerCell.parentElement.parentElement.parentElement;
+                const headerIndex = Array.prototype.indexOf.call(
+                    headerCell.parentElement.children,
+                    headerCell
+                );
+                const currentIsAscending =
+                    headerCell.classList.contains("th-sort-asc");
+
+                sortTableByColumn(
+                    tableElement,
+                    headerIndex,
+                    !currentIsAscending
+                );
+            });
+        });
+    }, []);
+
     return (
         <section id="section__introduce">
             <div id="box_table">
@@ -212,20 +366,36 @@ function TLTaiSanDDTU() {
                     <caption>Bảng</caption>
                     <thead>
                         <tr>
-                            <th>Mã phòng</th>
-                            <th>Tên phòng</th>
+                            <th>Mã Đồ</th>
+                            <th>Tên Đồ</th>
+                            <th>Mã Phòng</th>
+                            <th>Thông Số KT</th>
+                            <th>Nơi SX</th>
+                            <th>ĐVT</th>
+                            <th>DNTL</th>
+                            <th>Giải Trình</th>
+                            <th>Giải TS</th>
+                            <th>Thành Tiền</th>
                         </tr>
                     </thead>
                     <tbody>
                         {loading ? (
                             <tr>
-                                <td colSpan="2">Loading...</td>
+                                <td colSpan="1">Loading...</td>
                             </tr>
                         ) : (
                             data.map((item, index) => (
                                 <tr key={index}>
-                                    <td>{item.MAPH}</td>
-                                    <td>{item.TENP}</td>
+                                    <td>{item.MADO || 'N/A'}</td>
+                                    <td>{item.TENDO || 'N/A'}</td>
+                                    <td>{item.MAPX || 'N/A'}</td>
+                                    <td>{item.TSKT || 'N/A'}</td>
+                                    <td>{item.NOISX || 'N/A'}</td>
+                                    <td>{item.DVI || 'N/A'}</td>
+                                    <td>{item.DNTL || 'N/A'}</td>
+                                    <td>{item.GIAITRINH || 'N/A'}</td>
+                                    <td>{item.GIAITS || 'N/A'}</td>
+                                    <td>{item.THANHTIEN ? item.THANHTIEN.toLocaleString() : '0'}</td>
                                 </tr>
                             ))
                         )}
